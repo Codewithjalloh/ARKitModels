7d71e9682727da4bdbfcce6b473eb76c626099af
CodeWithJalloh <codewithjalloh@gmail.com>
2017-07-13 22:16:46 +0100

    lamp Button IBAction added

---
+++ ARKitModels/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  ARKitModels
+//
+//  Created by wealthyjalloh on 13/07/2017.
+//  Copyright © 2017 CWJ. All rights reserved.
+//
+
+import Foundation
+
+
+struct CameraCoordinate {
+    
+    var x = Float()
+    var y = Float()
+    var z = Float()
+    
+}
--- ARKitModels/ViewController.swift
+++ ARKitModels/ViewController.swift
@@ -17,12 +17,54 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let configuration = ARWorldTrackingSessionConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
+        
+        
     }
     
     
     
+    // get cam coordinates
+    func getYourCameraCoordinate(sceneView: ARSCNView) -> CameraCoordinate {
+        
+        let currentCameraTransform = sceneView.session.currentFrame?.camera.transform
+        
+        let cameraCoordinates = MDLTransform(matrix: currentCameraTransform!)
+        
+        var camCoord = CameraCoordinate()
+        
+        camCoord.x = cameraCoordinates.translation.x
+        camCoord.y = cameraCoordinates.translation.y
+        camCoord.z = cameraCoordinates.translation.z
+        
+        
+        return camCoord
+        
+        
+    }
+    
+    // lamp
     @IBAction func lampButtonPressed(_ sender: Any) {
+        let lampNode = SCNNode()
+        let camCoord = getYourCameraCoordinate(sceneView: sceneView)
+        lampNode.position = SCNVector3(camCoord.x, camCoord.y, camCoord.z)
+        
+        guard let modelObjScene = SCNScene(named:"lamp.scn", inDirectory: "Models.scnassets/lamp" ) else {
+            return
+        }
+        
+        let coverNode = SCNNode()
+        for child in modelObjScene.rootNode.childNodes {
+            child.geometry?.firstMaterial?.lightingModel = .physicallyBased
+            coverNode.addChildNode(child)
+        }
+        
+        lampNode.addChildNode(coverNode)
+        sceneView.scene.rootNode.addChildNode(lampNode)
+        
         
     }
     
